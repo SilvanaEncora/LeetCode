5172c2164f44809c36dd48d606792fe8f14ca757
RaulErnesto08 <ernesto.villarreal@encora.com>
2024-06-20 13:13:54 -0600

    Add Java Problem 2

---
+++ Java/Problem2.java
@@ -0,0 +1,19 @@
+package Java;
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long maxSum = 1;
+        int patches = 0;
+        int i = 0;
+        
+        while(maxSum <= n) {
+            if (i < nums.length && nums[i] <= maxSum) {
+                maxSum += nums[i];
+                i++;
+            } else {
+                maxSum += maxSum;
+                patches++;
+            }
+        }
+        return patches;
+    }
+}
